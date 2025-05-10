89c53c50796f1e7160cf5794600fedade9bb4243
Pasan-Anupama <e19091@eng.pdn.ac.lk>
2025-05-10 11:29:37 +0530

    Added multi-class classification to another repo and rename a png name

--- ecgMultiClassificationModel/preProcessing/Normalization.py
+++
@@ -1,7 +0,0 @@
-# Contains the code for normalization
-
-import numpy as np
-
-def normalize_beats(beats):
-    """Normalize each beat independently"""
-    return np.array([(beat - np.mean(beat))/np.std(beat) for beat in beats])
